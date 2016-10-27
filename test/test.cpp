7c0faabd6d1a31f954f2a7b1a54ca42e9fcb97ff
AxelStrem <axelstrem@gmail.com>
2016-10-27 13:39:42 +0300

    more tests

--- test/test.cpp
+++ test/test.cpp
@@ -6,13 +6,57 @@
 
 using namespace lptr;
 
+class TestIntAllocator
+{
+	static int value;
+public:
+	static char* allocate(size_t size)
+	{
+		return (char*)(&value);
+	}
+	static void free(char* ptr)
+	{
+
+	}
+};
+
+int TestIntAllocator::value = 0;
+
+static int TestIntAllocator::value = 0;
+
 TEST_CASE("basic tests", "[file]")
 {
 	SECTION("make_local")
 	{
 		auto p = make_local<int>(10);
-		std::cout<<"p = "<<(*p)<<std::endl;
+		int a = (*p);
+		int* ip = p.get();
+		auto p1 = p;
+		auto p2 = p;
 
-		REQUIRE(1 == 1);
+		REQUIRE(p.use_count() == 3);
+	}
+	
+	SECTION("allocate_local")
+	{
+		auto p = allocate_local<int, TestIntAllocator>(5);
+		int a = (*p);
+		int* ip = p.get();
+		auto p1 = p;
+
+		REQUIRE(p.unique() != true);
+	}
+	
+	SECTION("make_intrusive")
+	{
+		auto p = make_intrusive<int>(10);
+		int a = (*p);
+		int* ip = p.get();
+		
+		{
+			auto p2 = p;
+		}
+
+		REQUIRE(p.unique() == true);
 	}
 }
